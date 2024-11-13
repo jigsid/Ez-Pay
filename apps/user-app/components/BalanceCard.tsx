d6c06948597b0c14648ab073deea05c36119f9f0
jigsaw <jigsawsmma@gmail.com>
2024-11-13 15:36:08 +0530

    logo

--- apps/user-app/components/BalanceCard.tsx
+++ apps/user-app/components/BalanceCard.tsx
@@ -14,19 +14,19 @@ export const BalanceCard = ({
       <div className="flex justify-between items-center border-b border-slate-200 pb-3">
         <div className="text-gray-600 text-sm">Unlocked Balance</div>
         <div className="text-2xl font-bold text-green-500">
-          {(amount / 100).toFixed(2)} GBP
+          ${(amount / 100).toFixed(2)}
         </div>
       </div>
       <div className="flex justify-between items-center border-b border-slate-200 py-3">
         <div className="text-gray-600 text-sm">Locked Balance</div>
         <div className="text-2xl font-bold text-red-500">
-          {(locked / 100).toFixed(2)} GBP
+          ${(locked / 100).toFixed(2)}
         </div>
       </div>
       <div className="flex justify-between items-center pt-3">
         <div className="text-gray-600 text-sm">Total Balance</div>
         <div className="text-3xl font-bold text-indigo-500">
-          {totalBalance.toFixed(2)} GBP
+          ${totalBalance.toFixed(2)}
         </div>
       </div>
     </Card>
